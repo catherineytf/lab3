ae57f34260322f0aa1badd619094328d88cf90fd
catherineytf <114609344+catherineytf@users.noreply.github.com>
2022-10-14 12:25:37 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -22,7 +22,19 @@ public class ArrayTests {
   @Test
   public void testReverseInPlace2(){
     int[] input = {1,3,4,5,6,9};
-    assertArrayEquals(new int[]{9,6,5,4,3,1}, ArrayExamples.reverseInPlace(input));
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{9,6,5,4,3,1}, input);
+
+  }
+
+  @Test
+  public void testaverageWithoutLowest(){
+    double[] input1 = {3,4,6,1,2};
+    double[] input2 = {1};
+    double[] input3 = {1,2,3,1,1};
+    assertEquals(3.75,ArrayExamples.averageWithoutLowest(input1),0.001);
+    assertEquals(0.0,ArrayExamples.averageWithoutLowest(input2),0.001);
+    assertEquals(2.5,ArrayExamples.averageWithoutLowest(input3),0.001);
 
   }
 }
